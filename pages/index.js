e2d5f9b51ac224cf9ed4aea5201320005838ad38
Enrige13 <55575601+Enrige13@users.noreply.github.com>
2021-06-12 22:35:03 +0200

    #5: Creating a Layout Component

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,12 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque unde, vero deleniti adipisci voluptatum saepe ratione fuga alias, provident dolore nesciunt repellendus dolor odio sit voluptas et ipsa molestias ullam.</p>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A mollitia debitis aliquid alias id qui voluptas dolores non, fuga blanditiis, magni nisi repellat veniam ad nam sequi? Sunt, corrupti incidunt?</p>
       <Link href="/ninjas">
         <a>See Ninja Listing</a>
       </Link>
-      <Footer />
     </div>
   )
 }
