2e3cdb86339383f8cf84775d045f8461ba0eeb01
Enrige13 <55575601+Enrige13@users.noreply.github.com>
2021-06-12 22:22:09 +0200

    #3: Adding Other Components

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Navbar from '../comps/Navbar'
+import Footer from '../comps/Footer'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque unde, vero deleniti adipisci voluptatum saepe ratione fuga alias, provident dolore nesciunt repellendus dolor odio sit voluptas et ipsa molestias ullam.</p>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A mollitia debitis aliquid alias id qui voluptas dolores non, fuga blanditiis, magni nisi repellat veniam ad nam sequi? Sunt, corrupti incidunt?</p>
+      <Footer />
     </div>
   )
 }
