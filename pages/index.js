d3e452fc6492606c495dfdda0933116ceaf2d62c
Enrige13 <55575601+Enrige13@users.noreply.github.com>
2021-06-12 22:28:13 +0200

    #4: Linking Between Pages

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Navbar from '../comps/Navbar'
 import Footer from '../comps/Footer'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -11,6 +12,9 @@ export default function Home() {
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque unde, vero deleniti adipisci voluptatum saepe ratione fuga alias, provident dolore nesciunt repellendus dolor odio sit voluptas et ipsa molestias ullam.</p>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A mollitia debitis aliquid alias id qui voluptas dolores non, fuga blanditiis, magni nisi repellat veniam ad nam sequi? Sunt, corrupti incidunt?</p>
+      <Link href="/ninjas">
+        <a>See Ninja Listing</a>
+      </Link>
       <Footer />
     </div>
   )
