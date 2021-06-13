529fada4cfaf430341887519a91f73bb90c28e16
Enrige13 <55575601+Enrige13@users.noreply.github.com>
2021-06-13 12:54:02 +0200

    #8: Redirecting Users

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,18 @@
 import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 const NotFound = () => {
+    const router = useRouter()
+
+    useEffect(() => {
+        // console.log('use effect ran')
+        setTimeout(() => {
+            // router.go(-1) // browser history
+            router.push('/')
+        }, 3000)
+    }, [])
+
     return ( 
         <div className="not-found">
             <h1>Oooops...</h1>
