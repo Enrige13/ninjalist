689520a49132bb394565e276884418ad6e6b266e
Enrige13 <55575601+Enrige13@users.noreply.github.com>
2021-06-13 13:08:15 +0200

    #10: Fetching Data (getStaticProps)

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,12 +1,30 @@
 // localhost:3000/folder/ - creates root path only with (index.js)
-// import ninjas from '../styles/Ninjas.module.css'
+import styles from '../../styles/Jobs.module.css'
 
-const Ninjas = () => {
-    return ( 
-        <div>
-            <h1>All Ninjas</h1>
+export const getStaticProps = async () => { // run at build time - before component is rendered, never write code witch renders in the browser
+  const res = await fetch('https://jsonplaceholder.typicode.com/users') // await because it is async
+  const data = await res.json()
+
+  return { // return value object
+    props: { ninjas: data }
+  }
+}
+
+const Ninjas = ({ ninjas }) => {
+  console.log(ninjas)
+
+  return (
+    <div>
+      <h1>All Ninjas</h1>
+      {ninjas.map(ninja => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ ninja.name }</h3>
+          </a>
         </div>
-    );
+      ))}
+    </div>
+  )
 }
  
 export default Ninjas
